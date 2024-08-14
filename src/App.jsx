386929f19a0aadc21f18aab6a6709866950c9a47
Anany717 <134945045+Anany717@users.noreply.github.com>
2024-08-14 11:08:22 -0400

    New route App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { Link } from "react-router-dom";
 import SingleEmployeeContainer from './SingleEmployeeContainer';
+import SingleTaskContainer from './SingleTaskContainer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Link to={`employees`}><button>All Employees</button></Link>
       <Link to={`tasks`}><button>All Tasks</button></Link>
       <Route path="/employees/:employeeId" element={<SingleEmployeeContainer />} />
+      <Route path="/tasks/:taskId" element={<SingleTaskContainer />} />
     </>
   );
 }
