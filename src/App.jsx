3b2de77890fb9c46b28a434fba32db1d73273b0c
Anany717 <134945045+Anany717@users.noreply.github.com>
2024-08-14 21:33:53 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import AllEmployeesContainer from './components/containers/AllEmployeesContainer';
-import AllTasksContainer from './components/containers/AllTasksContainer';
-import SingleEmployeeContainer from './components/containers/SingleEmployeeContainer';
-import SingleTaskContainer from './components/containers/SingleTaskContainer';
-import NewEmployeeForm from './components/containers/NewEmployeeForm';
-import NewTaskContainer from './components/containers/NewTaskContainer';
-import EditEmployeeForm from './components/containers/EditEmployeeForm';
-import EditTaskForm from './components/containers/EditTaskForm';
-
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/employees" component={AllEmployeesContainer} />
-        <Route exact path="/tasks" component={AllTasksContainer} />
-        <Route exact path="/employees/new" component={NewEmployeeForm} />
-        <Route exact path="/tasks/new" component={NewTaskContainer} />
-        <Route exact path="/employees/:employeeId/edit" component={EditEmployeeForm} /> 
-        <Route exact path="/tasks/:taskId/edit" component={EditTaskForm} />
-        <Route path="/employees/:employeeId" component={SingleEmployeeContainer} />
-        <Route path="/tasks/:taskId" component={SingleTaskContainer} />
+      <h1>WELCOME</h1>
+      <Link to={`employees`}><button>All Employees</button></Link>
+      <Link to={`tasks`}><button>All Tasks</button></Link>
     </>
   );
 }
